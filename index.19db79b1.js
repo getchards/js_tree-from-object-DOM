a4e5bf8bcb0d4ec15344b1cf8fb5eb86bc9ff649
getchards <kazansasha02@gmail.com>
2024-06-12 23:49:28 +0300

    make build

---
+++ index.19db79b1.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var l=document.createElement("li");l.textContent=a,"object"==typeof t[a]&&e(l,t[a]),r.appendChild(l)}n.appendChild(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.19db79b1.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,31 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const ul = document.createElement("ul");
+    for(const product in data){
+        const li = document.createElement("li");
+        li.textContent = product;
+        if (typeof data[product] === "object") createTree(li, data[product]);
+        ul.appendChild(li);
+    }
+    element.appendChild(ul);
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
